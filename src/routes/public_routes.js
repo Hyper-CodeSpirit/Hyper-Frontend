c47cf1597c1df437a188c1bba0a96d4301d36ce0
Shenal Akalanka <shenalakalanka513@gmail.com>
2025-03-02 20:51:48 +0530

    init authentication pages

--- src/routes/public_routes.js
+++ src/routes/public_routes.js
@@ -1,11 +1,16 @@
 import { Route } from "react-router-dom";
 import Homepage from "../pages/homepage/Homepage";
+import LoginPage from "../pages/authentication/login/Login";
+import RegisterPage from "../pages/authentication/register/Register";
+
 
 function PublicRoutes() {
     return (
         <>
-            <Route path="/home" element={<Homepage />} />
-        </>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/signin" element={<LoginPage />} />
+            <Route path="/signup" element={<RegisterPage />} />
+            </>
     );
 }
 
